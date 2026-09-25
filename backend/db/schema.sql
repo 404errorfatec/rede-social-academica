8d46010d697624c14940e57686d0480f0f21ea57
Iuri Branquinho <iuribranquinho1@gmail.com>
2026-09-25 16:33:51 -0300

    adicionar tabelas de mapa de sala e agendamento

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -51,22 +51,22 @@ CREATE TABLE IF NOT EXISTS mural_avisos (
 
 --Mapa de salas: Mostra informaçoes  em tempo real
 CREATE TABLE IF NOT EXISTS mapa_de_salas(
-                                            id SERIAL PRIMARY KEY,
-                                            sala INTEGER NOT NULL,
-                                            tipo TEXT NOT NULL,
-                                            andar INTEGER NOT NULL,
-                                            lotacao NUMERIC NOT NULL,
-                                            ocupacao BOOLEAN NOT NULL,
+  id SERIAL PRIMARY KEY,
+  sala INTEGER NOT NULL,
+  tipo TEXT NOT NULL,
+  andar INTEGER NOT NULL,
+  lotacao NUMERIC NOT NULL,
+  ocupacao BOOLEAN NOT NULL,
 );
 --Agendamento--
 CREATE TABLE IF NOT EXISTS agendamento(
-                                          id SERIAL PRIMARY KEY,
-                                          dia DATE NOT NULL,
-                                          data_final DATE NOT NULL,
-                                          horario TIME NOT NULL,
-                                          disponibilidade BOOLEAN NOT NULL,
-                                          responsavel TEXT NOT NULL,
-                                          sala NUMERIC NOT NULL,
+  id SERIAL PRIMARY KEY,
+  dia DATE NOT NULL,
+  data_final DATE NOT NULL,
+  horario TIME NOT NULL,
+  disponibilidade BOOLEAN NOT NULL,
+  responsavel TEXT NOT NULL,
+  sala NUMERIC NOT NULL,
 );
 
 -- Placeholders para próximas etapas do fluxo (Chat e Mapa de Salas)
